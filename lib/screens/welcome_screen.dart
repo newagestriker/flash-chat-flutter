fcdb86b084bb0949460ea96da2513f51a256db94
Sk Ramizuddin <newagestriker@gmail.com>
2020-05-02 02:45:22 +0530

    Create All Screens

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,60 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import '../components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+  
+  AnimationController animationController;
+  Animation logoAnimation,screenColorAnimation;
+  @override
+  void initState() {
+
+    super.initState();
+    
+    animationController = new AnimationController(vsync: this,duration: Duration(seconds: 1));
+
+    screenColorAnimation = ColorTween(begin: Colors.lightBlueAccent,end: Colors.white).animate(animationController);
+    logoAnimation = CurvedAnimation(parent: animationController, curve: Curves.easeIn);
+
+    animationController.forward(from: 0.0);
+    animationController.addListener((){
+
+      setState(() {
+
+      });
+
+    });
+
+    animationController.addStatusListener((status){
+
+      if(status==AnimationStatus.completed){
+        animationController.reverse(from: 1.0);
+      }
+      else if(status == AnimationStatus.dismissed){
+        animationController.forward(from: 0.0);
+      }
+    });
+    
+
+  }
+  @override
+  void dispose() {
+
+    animationController.dispose();
+    
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: screenColorAnimation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,9 +63,11 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0*logoAnimation.value,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
@@ -34,45 +81,17 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(color: Colors.lightBlueAccent,title: 'Log In',onPressed: () {
+              Navigator.pushNamed(context, LoginScreen.id);//Go to login screen.
+            }),
+            RoundedButton(color: Colors.blueAccent,title: 'Register',onPressed: () {
+              Navigator.pushNamed(context, RegistrationScreen.id);//Go to login screen.
+            }),
           ],
         ),
       ),
     );
   }
 }
+
+
